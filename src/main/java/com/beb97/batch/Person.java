c2da1fc247f7ed5d74f8fb2407623eabcfc16da1
unknown <beb97@hotmail.com>
2020-08-06 12:39:12 +0200

    Revert "Ajout de Person.mail + classe Pret"
    This reverts commit c3ce489740d3aec6160bac36a0e602d206108288.
    
    revert commit pret

--- src/main/java/com/beb97/batch/Person.java
+++ src/main/java/com/beb97/batch/Person.java
@@ -5,20 +5,12 @@ public class Person {
     private String lastName;
     private String firstName;
 
-    private String email;
-
     public Person() {
     }
 
     public Person(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-    }
-
-    public Person(String firstName, String lastName, String email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
     }
 
     public void setFirstName(String firstName) {
@@ -37,17 +29,9 @@ public class Person {
         this.lastName = lastName;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     @Override
     public String toString() {
-        return "firstName: " + firstName + ", lastName: " + lastName + ", email: " + email;
+        return "firstName: " + firstName + ", lastName: " + lastName;
     }
 
 }
